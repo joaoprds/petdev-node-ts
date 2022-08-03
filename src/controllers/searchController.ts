228310352d612881b8e72bb72dfaac1c6e37a1e6
joaoprds <joaopaulorochadesousa@gmail.com>
2022-08-03 16:20:01 -0300

    feat:final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,22 @@
 import { Request, Response} from 'express';
+import { Pet } from '../models/pet';
+import {createMenuObject} from '../helpers/createMenuObjects'
+import { request } from 'http';
 
 export const search = (req: Request, res: Response) => {
+    let query: string = req.query.q as string;
 
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+
+    })
 
 };
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(MainRoutes);
 
 server.use((req, res) => {
-    res.status(404).send('Página não encontrada');
+    res.render('pages/404');
 });
 
 //Rotas
